74722a99822eccabf5a421cba91f51262c0c76a8
IsraelRobTheProgrammer <israelrobinson965@gmail.com>
2025-01-18 17:09:25 +0100

    Build & Changes: used the django command management system to write a new command which will wai_for_db before starting the main app in our container

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,28 @@
+"""
+Django Commands to be used with the manage.py module
+"""
+
+from django.core.management.base import BaseCommand
+import time
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    """Command to wait for DB to be available"""
+
+    def handle(self, *args, **options):
+        """Entrypoint for command."""
+        self.stdout.write("Waiting for DB...")
+        db_up = False
+        while db_up is False:
+            try:
+                print("about to check_db")
+                self.check(databases=["default"])
+                db_up = True
+            except (OperationalError, Psycopg2Error) as e:
+                self.stdout.write("DB unavailable waiting 1 second")
+                time.sleep(1)
+                print(e, "operational_error")
+                print(e, "psycopg2_error")
+        self.stdout.write(self.style.SUCCESS("DB available!"))
